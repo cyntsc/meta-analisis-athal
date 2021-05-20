361dc2712c414a78d60afda0f4f7610e753d7a65
cyntsc <bioinformatic2019@gmail.com>
2021-05-20 17:54:17 -0500

    Adding grep utilities for alignments and quantifications statistics for athal healthy and infected

--- grep_utilities/my_statistics_HTSeq_HEALTHY.sh
+++ grep_utilities/my_statistics_HTSeq_HEALTHY.sh
@@ -1,9 +1,6 @@
-# Made by. Cynthia SC
-# Get some statistics from all HTSeq output reports set in a specific directory. 
 
-# IN: Just go to the <dir> where are the HTSeq output reports and run the code.
-# OUT: A file with all the lines including the specified pattern.
- 
+# Get the statistics of the counts 
+
 > my_statistics_HTSeq_counts.txt
 grep 'no_feature' SRR* | sort   >> my_statistics_HTSeq_counts.txt 
 grep 'ambiguous' SRR* | sort  >> my_statistics_HTSeq_counts.txt
